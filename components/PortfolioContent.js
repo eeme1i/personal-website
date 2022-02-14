1716cdd0d2336ab5f2dbff8f06e27336c5d73e7a
eemeli <eemeliruoh@gmail.com>
2022-02-14 19:32:18 +0200

    fix bg scaling

--- components/PortfolioContent.js
+++ components/PortfolioContent.js
@@ -2,12 +2,15 @@ import Link from "next/link";
 
 function PortfolioContent() {
     return (
-    <div className='text-4xl w-full my-8 md:my-0 md:w-2/3'>
-        <div className="grid grid-cols-1 xl:grid-cols-2 gap-4">
+    <div className='text-4xl h-full w-full my-8 md:my-0 md:w-2/3'>
+            <div className='text-4xl w-full my-8 md:my-0 md:w-2/3 pb-8'>
+                <h1>Portfolio</h1>
+            </div>
+        <div className="h-full grid grid-cols-1 xl:grid-cols-2 gap-4 pb-8">
             <div className="bg-blue-400 h-64"></div>
             <div className="bg-blue-400 h-64"></div>
-            <div className="bg-blue-400"></div>
-            <div className="bg-blue-400"></div>
+            <div className="bg-blue-400 h-64"></div>
+            <div className="bg-blue-400 h-64"></div>
         </div>
     </div>
     )
