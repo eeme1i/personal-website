9e87eaf046a25a3ff359ecb7805db99456739065
eemeli ruohomäki <56453212+russhh@users.noreply.github.com>
2022-02-17 14:44:22 +0200

    stuff portfolio is ready

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,25 +1,31 @@
 import Link from "next/link";
 
 function Sidebar() {
-    return (
-        <div className='w-full md:w-2/4 xl:w-1/4 flex md:block bg-gray-900 rounded-3xl p-8 m-auto justify-center mt-0'>
-          <div className='md:mb-4'>
-            <Link href='/'>
-            <a className='px-4 hover:py-2 rounded-lg hover:bg-gray-800 transition-all duration-150 ease-linear'>Index</a>
-            </Link>
-          </div>
-          <div className='md:mb-4'>
-            <Link href='/portfolio'>
-            <a className='px-4 hover:py-2 rounded-lg hover:bg-gray-800 transition-all duration-150 ease-linear'>Portfolio</a>
-            </Link>
-          </div>
-          <div className=''>
-            <Link href='/contact'>
-            <a className='px-4 hover:py-2 rounded-lg hover:bg-gray-800 transition-all duration-150 ease-linear'>Contact</a>
-            </Link>
-          </div>
+  return (
+    <div className="m-auto mt-0 flex w-full justify-center rounded-3xl bg-gray-900 p-8 md:block md:w-2/4 lg:w-1/4 xl:w-1/4">
+      <div className="md:mb-4">
+        <Link href="/">
+          <a className="rounded-lg px-4 transition-all duration-150 ease-linear hover:bg-gray-800 hover:py-2">
+            Index
+          </a>
+        </Link>
       </div>
-    )
+      <div className="md:mb-4">
+        <Link href="/portfolio">
+          <a className="rounded-lg px-4 transition-all duration-150 ease-linear hover:bg-gray-800 hover:py-2">
+            Portfolio
+          </a>
+        </Link>
+      </div>
+      <div className="">
+        <Link href="/contact">
+          <a className="rounded-lg px-4 transition-all duration-150 ease-linear hover:bg-gray-800 hover:py-2">
+            Contact
+          </a>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
