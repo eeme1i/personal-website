0fef467682596d235d5ee14ceda8a63ad2d8f457
eemeli <eemeliruoh@gmail.com>
2022-02-18 13:53:48 +0200

    fix index

---
+++ components/IndexContent.js
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import Image from "next/image";
+
+function IndexContent() {
+  return (
+    <div className="z-1 flex min-h-full w-full sm:justify-end">
+      <div className="my-8 w-full text-4xl sm:relative md:my-0 md:w-2/3">
+        <div className="w-full pb-8 text-4xl md:my-0 md:w-2/3">
+          <h1>Index</h1>
+        </div>
+        <div className="text-xl">
+          <p>Index</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default IndexContent;
