10c46e3a609db2f8deb7e9d8df2ae517ec6d676a
eemeli ruohomäki <56453212+eeme1i@users.noreply.github.com>
2022-02-20 15:17:24 +0200

    fix titles, and contact info

--- components/ContactContent.js
+++ components/ContactContent.js
@@ -8,54 +8,74 @@ function ContactContent() {
         <div className="w-full pb-8 text-4xl md:my-0 md:w-2/3">
           <h1>Contact</h1>
         </div>
-        <div className="text-xl">
-          <div className="mb-2">
-            <Link href="https://twitter.com/hieemeli">
-              <a
-                target="_blank"
-                rel="noreferrer"
-                className="flex hover:underline"
-              >
-                Twitter
-                <Image
-                  src="/north_east_white_24dp.svg"
-                  width="24px"
-                  height="24px"
-                />
-              </a>
-            </Link>
+        <div className="text-xl sm:flex sm:space-x-8">
+          <div>
+            <div className="mb-2">
+              <Link href="https://twitter.com/hieemeli">
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  className="flex hover:underline"
+                >
+                  Twitter
+                  <Image
+                    src="/north_east_white_24dp.svg"
+                    width="24px"
+                    height="24px"
+                  />
+                </a>
+              </Link>
+            </div>
+            <div className="mb-2">
+              <Link href="https://github.com/eeme1i">
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  className="flex hover:underline"
+                >
+                  Github
+                  <Image
+                    src="/north_east_white_24dp.svg"
+                    width="24px"
+                    height="24px"
+                  />
+                </a>
+              </Link>
+            </div>
           </div>
-          <div className="mb-2">
-            <Link href="https://github.com/eeme1i">
-              <a
-                target="_blank"
-                rel="noreferrer"
-                className="flex hover:underline"
-              >
-                Github
-                <Image
-                  src="/north_east_white_24dp.svg"
-                  width="24px"
-                  height="24px"
-                />
-              </a>
-            </Link>
-          </div>
-          <div className="mb-2">
-            <Link href="https://insagram.com/eemelihq">
-              <a
-                target="_blank"
-                rel="noreferrer"
-                className="flex hover:underline"
-              >
-                Instagram
-                <Image
-                  src="/north_east_white_24dp.svg"
-                  width="24px"
-                  height="24px"
-                />
-              </a>
-            </Link>
+          <div>
+            <div className="mb-2">
+              <Link href="https://insagram.com/eemelihq">
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  className="flex hover:underline"
+                >
+                  Instagram
+                  <Image
+                    src="/north_east_white_24dp.svg"
+                    width="24px"
+                    height="24px"
+                  />
+                </a>
+              </Link>
+            </div>
+            <div className="mb-2">
+              <Link href="mailto:eeruoh@gmail.com">
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  className="flex hover:underline"
+                >
+                  Mail
+                  <Image
+                    src="/north_east_white_24dp.svg"
+                    width="24px"
+                    height="24px"
+                  />
+                </a>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
