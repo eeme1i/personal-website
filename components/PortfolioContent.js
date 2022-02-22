96e43e26fd738fb183b878104cf32fc1db57120f
eemeli ruohomäki <56453212+eeme1i@users.noreply.github.com>
2022-02-22 15:40:36 +0200

    idk

--- components/PortfolioContent.js
+++ components/PortfolioContent.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-function PortfolioContent() {
+function PortfolioContentB() {
   return (
     <div className="z-1 mb-8 flex min-h-full w-full justify-end">
       <div className="bg-Greener relative my-8 w-full rounded-3xl p-8 text-4xl md:my-0 md:w-2/3">
@@ -29,12 +29,12 @@ function PortfolioContent() {
             </div>
           </div>
         </div>
-        <div className="">
+        <div className="text-Zircon text-lg sm:text-2xl">
           <Link href="https://www.instagram.com/p/CZmcNyZsuqG/">
             <a className="h-full w-full">
               <div className="mb-8 h-96 rounded-3xl bg-[url('/01.jpg')] bg-cover bg-center">
                 <div className="bg-Firefly/[0.6] h-full w-full rounded-3xl opacity-0 transition-all duration-150 ease-linear hover:opacity-100">
-                  <div className="text-Zircon flex justify-between p-8">
+                  <div className="flex justify-between p-8">
                     <p className="">01.png</p>
                     <p>2022</p>
                   </div>
@@ -46,7 +46,7 @@ function PortfolioContent() {
             <a className="h-full w-full">
               <div className="mb-8 h-96 rounded-3xl bg-[url('/02.jpg')] bg-cover bg-center">
                 <div className="bg-Firefly/[0.6] h-full w-full rounded-3xl opacity-0 transition-all duration-150 ease-linear hover:opacity-100">
-                  <div className="text-Zircon flex justify-between p-8">
+                  <div className="flex justify-between p-8">
                     <p className="">s-4-pro.png</p>
                     <p>2022</p>
                   </div>
@@ -58,7 +58,7 @@ function PortfolioContent() {
             <a className="h-full w-full">
               <div className="h-96 rounded-3xl bg-[url('/03.jpg')] bg-cover bg-center">
                 <div className="bg-Firefly/[0.6] h-full w-full rounded-3xl opacity-0 transition-all duration-150 ease-linear hover:opacity-100">
-                  <div className="text-Zircon flex justify-between p-8">
+                  <div className="flex justify-between p-8">
                     <p className="">Vendetta</p>
                     <p>2022</p>
                   </div>
@@ -66,10 +66,13 @@ function PortfolioContent() {
               </div>
             </a>
           </Link>
+          <div className="text-Emerald flex w-full justify-center pt-6 text-sm">
+            <p>more coming soon :)</p>
+          </div>
         </div>
       </div>
     </div>
   );
 }
 
-export default PortfolioContent;
+export default PortfolioContentB;
